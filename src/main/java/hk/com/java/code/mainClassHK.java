8ef9b53fcefa91e6fc24d9bb311cd9e5a46b42df
Habib Keyrouz <habib.keyrouz@almasar-solutions.com>
2023-05-14 19:47:57 +0300

    file addedddd

---
+++ src/main/java/hk/com/java/code/mainClassHK.java
@@ -0,0 +1,14 @@
+package hk.com.java.code;
+
+public class mainClassHK {
+
+	public static String getFullName(String first, String last) {
+		
+		return first + " " + last ;
+	}
+	
+	public static String getFullPrtName(String first, String last, String midle) {
+		
+		return first + " " + last + " " +midle;
+	}
+}
